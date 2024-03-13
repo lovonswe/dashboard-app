df37e016a8613a8064567b7679021bbc73306a5a
nahid <nahidswe@gmail.com>
2024-03-13 19:18:39 +0600

    dashboard added.

---
+++ src/components/router.js
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "about",
+          element: <About />,
+        },
+      ],
+    },
+  ]);
